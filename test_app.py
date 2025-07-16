28608399f0939c6d3bb82a5ea0a8b78478c31f48
anandhanmanojkumar <anandhanmanojkumar@gmail.com>
2025-07-16 13:25:37 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -4,13 +4,9 @@ from app import app
 @pytest.fixture
 def client():
     app.testing = True
-    with app.test_client() as client:
-        yield client
+    return app.test_client()
 
-def test_home_status_code(client):
-    response = client.get('/')
-    assert response.status_code == 200
-
-def test_home_content(client):
-    response = client.get('/')
-    assert b"Welcome" in response.data  # Change this if your homepage uses different text
+def test_home(client):
+    res = client.get('/')
+    assert res.status_code == 200
+    assert b"Welcome" in res.data
